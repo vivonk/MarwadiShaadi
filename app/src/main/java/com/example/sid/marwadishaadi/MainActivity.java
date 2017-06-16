b94d3e40d972493694a79503c852241f26bea0eb
Siddhesh Rane <siddheshrane24@gmail.com>
2017-06-16 17:49:13 +0530

    intro

--- app/src/main/java/com/example/sid/marwadishaadi/MainActivity.java
+++ app/src/main/java/com/example/sid/marwadishaadi/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.sid.marwadishaadi;
-
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
 
 import com.example.sid.marwadishaadi.Intro_Slides.Intro1Fragment;
 import com.example.sid.marwadishaadi.Intro_Slides.Intro2Fragment;
-import com.github.paolorotolo.appintro.AppIntro;
-import com.github.paolorotolo.appintro.AppIntro2;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppIntro2 implements Intro1Fragment.OnFragmentInteractionListener,Intro2Fragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
 
-    @Override
+    private ViewPager mviewpager;
+
+   @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
@@ -26,59 +26,56 @@ public class MainActivity extends AppIntro2 implements Intro1Fragment.OnFragment
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        addSlide(new Intro1Fragment());
-        addSlide(new Intro2Fragment());
-        showStatusBar(false);
+        SliderAdapter sliderAdapter = new SliderAdapter(getSupportFragmentManager());
+        mviewpager = (ViewPager) findViewById(R.id.main_container);
+        mviewpager.setAdapter(sliderAdapter);
+        mviewpager.setOnPageChangeListener(this);
 
-        // Hide Skip/Done button.
-        showSkipButton(false);
-
-        setProgressButtonEnabled(true);
-
-        setFadeAnimation();
-
-        setVibrate(true);
-        setVibrateIntensity(30);
     }
 
     @Override
-    public void onSkipPressed(Fragment currentFragment) {
-        super.onSkipPressed(currentFragment);
-        // Do something when users tap on Skip button.
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
     }
 
     @Override
-    public void onDonePressed(Fragment currentFragment) {
-        super.onDonePressed(currentFragment);
+    public void onPageSelected(int position) {
 
-        SharedPreferences sharedpref=getSharedPreferences("userinfo",MODE_PRIVATE);
-        boolean check = sharedpref.getBoolean("isLoggedIn",false);
+    }
 
-        Log.d(":", "onDonePressed:--------------------------- bool is  "+check);
-        if(check){
-            Intent i = new Intent(MainActivity.this, Dashboard.class);
-            startActivity(i);
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            finish();
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public class SliderAdapter extends FragmentStatePagerAdapter {
+
+        public SliderAdapter(FragmentManager fm) {
+            super(fm);
         }
-        else {
-            Intent i = new Intent(MainActivity.this, Login.class);
-            startActivity(i);
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            finish();
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    Intro1Fragment intro1Fragment = new Intro1Fragment();
+                    return intro1Fragment;
+                case 1:
+                    Intro2Fragment intro2Fragment = new Intro2Fragment();
+                    return intro2Fragment;
+            }
+            return null;
         }
-    }
 
-    @Override
-    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
-        super.onSlideChanged(oldFragment, newFragment);
-        // Do something when the slide changes.
-    }
-
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
+        @Override
+        public int getCount() {
+            return 2;
+        }
 
     }
+
+
+
 }

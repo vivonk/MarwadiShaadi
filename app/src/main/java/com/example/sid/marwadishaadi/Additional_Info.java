510f13edd1b42ea3f6a2ba46fe0beddf3158404a
Mervin24 <mdalmet4@gmail.com>
2017-06-19 16:54:12 +0530

    time picker

--- app/src/main/java/com/example/sid/marwadishaadi/Additional_Info.java
+++ app/src/main/java/com/example/sid/marwadishaadi/Additional_Info.java
@@ -1,28 +1,73 @@
 package com.example.sid.marwadishaadi;
 
+import android.app.TimePickerDialog;
 import android.content.Context;
+import java.util.Calendar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
 
 public class Additional_Info extends Fragment implements Step{
 
+    private EditText dob;
     public Additional_Info() {
         // Required empty public constructor
+    }
+
+    private int pHour;
+    private int pMinute;
+
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    pHour = hourOfDay;
+                    pMinute = minute;
+                    updateDisplay();
+                }
+            };
+
+    private void updateDisplay() {
+        dob.setText(
+                new StringBuilder()
+                        .append(pad(pHour)).append(":")
+                        .append(pad(pMinute)));
+    }
+
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_additional__info, container, false);
+        View view = inflater.inflate(R.layout.fragment_additional__info, container, false);
+        dob  = (EditText)view.findViewById(R.id.dob);
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int min = c.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog =new TimePickerDialog(getContext(),mTimeSetListener,hour,min, DateFormat.is24HourFormat(getActivity()));
+                timePickerDialog.setTitle("Select Time");
+                timePickerDialog.show();
+            }
+        });
+        return view;
     }
 
     @Override

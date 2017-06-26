dee5f09cefb941794783e622c58d6f044856718d
maitreepasad <maitreepasad@gmail.com>
2017-06-26 11:27:46 +0530

    removed param

--- app/src/main/java/com/example/sid/marwadishaadi/User_Profile/ProfileAdditionalDetailsFragment.java
+++ app/src/main/java/com/example/sid/marwadishaadi/User_Profile/ProfileAdditionalDetailsFragment.java
@@ -119,22 +119,8 @@ public class ProfileAdditionalDetailsFragment extends Fragment {
     }
 
 
-  /*  @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }*/
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+
 
     private class ProfileAdditionalDetails extends AsyncTask<Void,Void,Void>{
         @Override
